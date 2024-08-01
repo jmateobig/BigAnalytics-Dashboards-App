bf00d7cf3d1a867e9b6823336c7594e84eae7ce4
Jonathan <jmateo@biganalytics.com.gt>
2024-08-01 13:12:29 -0600

    fix: :bug: add allowed hosts

--- publicacion/settings.py
+++ publicacion/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = tuple (env.list('ALLOWED_HOSTS', default=[]))
 
 
 # Application definition
