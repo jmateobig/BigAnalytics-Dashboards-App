447139a151d9c9d125af7eccc2d081795a72d096
Jonathan <jmateo@biganalytics.com.gt>
2024-08-09 11:22:11 -0600

    fix: :bug:
    se mejora la forma de desplegar los dashboards

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -4,26 +4,38 @@ from dashboard.models import Dashboard
 def dashboards_context(request):
     if request.user.is_authenticated:
         user = request.user
-        # Dashboards asignados directamente
+        user_groups = user.groups.all()
+        user_group_ids = user_groups.values_list('id', flat=True)
+
+        # Obtener dashboards asignados directamente al usuario
         direct_dashboards = Dashboard.objects.filter(permission__user=user)
-        
-        # Dashboards asignados por grupo
-        group_dashboards = Dashboard.objects.filter(permission__group__user=user).distinct()
-        
-        # Agrupar dashboards por grupo
-        group_dashboards_by_group = {}
-        for dashboard in group_dashboards:
-            groups = dashboard.permission.group_set.all()
-            for group in groups:
-                if group not in group_dashboards_by_group:
-                    group_dashboards_by_group[group] = []
-                group_dashboards_by_group[group].append(dashboard)
+
+        # Obtener dashboards asignados a los grupos del usuario
+        group_dashboards = Dashboard.objects.filter(permission__group__in=user_group_ids).distinct()
+
+        # Obtener IDs de dashboards que están asignados a grupos
+        group_dashboard_ids = group_dashboards.values_list('id', flat=True)
 
         # Filtrar dashboards directos eliminando los que ya están en group_dashboards
-        direct_dashboards = direct_dashboards.exclude(id__in=group_dashboards.values_list('id', flat=True))
+        direct_dashboards = direct_dashboards.exclude(id__in=group_dashboard_ids)
+
+        # Inicializar un diccionario para almacenar los dashboards por grupo
+        group_dashboards_by_group = {group: [] for group in user_groups}
+
+        # Asignar dashboards a cada grupo
+        for dashboard in group_dashboards:
+            # Obtener los grupos asociados a este dashboard
+            groups = dashboard.permission.group_set.filter(id__in=user_group_ids).order_by('id')
+            for group in groups:
+                group_dashboards_by_group[group].append(dashboard)
+                break
+
+        # Eliminar grupos que no contienen dashboards
+        group_dashboards_by_group = {group: dashboards for group, dashboards in group_dashboards_by_group.items() if dashboards}
 
         return {
             'direct_dashboards': direct_dashboards,
             'group_dashboards_by_group': group_dashboards_by_group
         }
+    
     return {}
